e49cdff04c84ea3451495cd8a173e9ce2ffb9c50
Maximilian Wittmann <Maximilian.Wittmann@cba.com.au>
2017-02-04 19:24:19 +1100

    First working test (no comicIds param specified) for ComicsController unit test

--- test/controllers/ComicsControllerSpec.scala
+++ test/controllers/ComicsControllerSpec.scala
@@ -1,15 +1,19 @@
 package controllers
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
 
 import org.scalatestplus.play.PlaySpec
 import services.{ComicsService, ComicsServiceImpl}
 import org.scalatest.mock.MockitoSugar
-import play.api.mvc.{AnyContent, Request}
+import play.api.mvc.{AnyContent, Request, Result}
 import play.api.test.FakeRequest
 import play.mvc.Http.RequestBuilder
 import play.api.libs.concurrent.Execution.Implicits._
 import org.mockito.Mockito._
+import play.api.http.HttpEntity
+import play.api.http.HttpEntity.{Chunked, Streamed, Strict}
+import play.api.test.Helpers._
 
 class ComicsControllerSpec extends PlaySpec {
   def setup(): (ComicsService, ComicsController) = {
@@ -24,8 +28,30 @@ class ComicsControllerSpec extends PlaySpec {
 
   "ComicsController GET" should {
     "return BadRequest if the comicsId parameter is not present in the url" in new Context {
-//      val rb = (new RequestBuilder()).uri("http://www.dontcare.com/comics?comicIds=1")
+      //Given
+      when(mockComicsService.get(Seq(1))) thenReturn(Future.successful(Seq()))
 
+      val req: Request[AnyContent] = FakeRequest(
+        method = "GET",
+        path = "http://www.whatevs.com/comics"
+      )
+
+      //When
+      val result = comicsController.comics()(req)
+
+      //Then
+      val content = contentAsString(result)
+      val rStatus: Int = status(result)
+
+      rStatus mustEqual(400)
+      content mustEqual("Include a url-encoded comicIds parameter whose value is a list of comma-separated comic ids")
+    }
+
+    "return BadRequest if the comicsId parameter cannot be parsed to a list of integers" in new Context {
+
+    }
+
+    "return Ok and a json result describing the result returned from ComicsService" in new Context {
       when(mockComicsService.get(Seq(1))) thenReturn(Future.successful(Seq()))
 
       val req: Request[AnyContent] = FakeRequest(
@@ -35,14 +61,6 @@ class ComicsControllerSpec extends PlaySpec {
 
       val r = comicsController.comics()(req)
       println(s"R is: $r")
-    }
-
-    "return BadRequest if the comicsId parameter cannot be parsed to a list of integers" in {
-
-    }
-
-    "return Ok and a json result describing the result returned from ComicsService" in {
-
     }
   }
 }
