71681d5e5e335b89687feecd8ba48a30734d9291
Maximilian Wittmann <Maximilian.Wittmann@cba.com.au>
2017-02-04 13:43:35 +1100

    First working test for ComicsService; make a request for three comics ids, respect back the relevant data.results part of the json returned by the mock WSClient

--- test/ComicsServiceSpec.scala
+++ test/ComicsServiceSpec.scala
@@ -17,6 +17,8 @@ import org.mockito.Mockito._
 import org.scalatest._
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play._
+import play.api.libs.json.{JsArray, JsDefined, JsNumber, JsObject, JsString}
+import play.libs.Json
 import services.ComicsService.ComicQueryResult
 
 class ComicsServiceSpec extends PlaySpec {
@@ -25,20 +27,29 @@ class ComicsServiceSpec extends PlaySpec {
     "pickle a george" in {
       //Given
       val mockMarvelService: MarvelService = MockitoSugar.mock[MarvelService]
-      val apiKeysPart: String = "?apiKeysPart=something"
+      val apiKeysPart: String = "apiKeysPart=something"
       when(mockMarvelService.apiKeysUrlPart) thenReturn apiKeysPart
 
-      val comic1Url = "http://gateway.marvel.com:80/v1/public/comics/1" + apiKeysPart
-      val comic2Url = "http://gateway.marvel.com:80/v1/public/comics/2" + apiKeysPart
+      val comic1Url = "http://gateway.marvel.com:80/v1/public/comics/1?" + apiKeysPart
+      val comic2Url = "http://gateway.marvel.com:80/v1/public/comics/2?" + apiKeysPart
+      val comic3Url = "http://gateway.marvel.com:80/v1/public/comics/3?" + apiKeysPart
+
+      //jsonBody \ "data" \ "results" \ 0
+      val validJson1 = createValidJson(1)
+      val validJson2 = createValidJson(2)
+      val validJson3 = createValidJson(3)
 
       val ws = MockWS {
-        case (GET, comic1Url) => Action { Ok("http response") }
-        case (GET, comic2Url) => Action { Ok("http response") }
+        case (GET, `comic1Url`) => Action { Ok(validJson1.jsonResponse) }
+        case (GET, `comic2Url`) => Action { Ok(validJson2.jsonResponse) }
+        case (GET, `comic3Url`) => Action { Ok(validJson3.jsonResponse) }
       }
 
       val comicsService = new ComicsService(ws, mockMarvelService)(play.api.libs.concurrent.Execution.Implicits.defaultContext)
       val expectedResult = Set(
-
+        ComicsService.Found(JsDefined(validJson1.queryPartOnly)),
+        ComicsService.Found(JsDefined(validJson2.queryPartOnly)),
+        ComicsService.Found(JsDefined(validJson3.queryPartOnly))
       )
 
       //When
@@ -48,5 +59,25 @@ class ComicsServiceSpec extends PlaySpec {
       //Then
       result mustEqual(expectedResult)
     }
+  }
+
+  case class MarvelResponse(jsonResponse: JsObject, queryPartOnly: JsObject)
+
+  def createValidJson(uniqueValue: Int): MarvelResponse = {
+    val results =
+      JsObject(Seq(
+        "fieldA" -> JsString("Hello"),
+        "fieldB" -> JsNumber(uniqueValue)
+      ))
+
+    val jsonResponse = JsObject(Seq(
+      "someField" -> JsString("SomeVal"),
+      "otherThing" -> JsArray(Seq(JsNumber(1), JsNumber(2))),
+      "data" -> JsObject(Seq(
+        "results" -> JsArray(Seq(results))
+      ))
+    ))
+
+    MarvelResponse(jsonResponse, results)
   }
 }
